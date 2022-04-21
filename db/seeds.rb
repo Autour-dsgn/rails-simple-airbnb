0cb810b15d3bdffdfb5b3ea6b6ef1c8681968a22
Aurelie Tournay <info@aurelietournay.com>
2022-04-21 20:45:20 +0200

    airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+Flat.destroy_all
+4.times do
+  flat = Flat.create!(name: Faker::Address.city,
+                      address: Faker::Address.street_address,
+                      description: Faker::Lorem.sentence,
+                      price_per_night: rand(90..500),
+                      number_of_guests: rand(1..6))
+  flat.save
+end
